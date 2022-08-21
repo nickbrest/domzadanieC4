f2e5f0a2ee5baaa8bf2c2abc3e7755dcf6e96af5
Nick <mikabrmail@gmail.com>
2022-08-22 00:41:53 +0300

    task3 complete

---
+++ task3/Program.cs
@@ -0,0 +1,45 @@
+﻿// Написать программу копирования массива
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    for (int i = 0; i < length; i++)
+    {
+        collection[i] = new Random().Next(1, 20);
+    }
+}
+
+void PrintArray(int[] collection)
+{
+    int length = collection.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write(collection[i] + " ");
+    }
+}
+
+void CopyArray(int[] collect1, int[] collect2)
+{
+    if (collect1.Length > collect2.Length)
+        for (int i = 0; i < collect2.Length; i++)
+        {
+            collect2[i] = collect1[i];
+        }
+    else
+    {
+        for (int i = 0; i < collect1.Length; i++)
+        {
+            collect2[i] = collect1[i];
+        }
+    }
+}
+
+int[] firstArray = new int[20];
+int[] secondArray = new int[20];
+
+FillArray(firstArray);
+PrintArray(firstArray);
+Console.WriteLine();
+CopyArray(firstArray, secondArray);
+PrintArray(secondArray);
+
