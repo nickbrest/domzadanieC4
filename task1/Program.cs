eadb6893517495442ff47d1e8dd34e0c464c8f81
Nick <mikabrmail@gmail.com>
2022-08-22 00:41:17 +0300

    task1 complete

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+
+double b1 = 10;
+double k1 = 7;
+double b2 = 2;
+double k2 = 6;
+
+if (k1 == k2) Console.WriteLine("Прямые параллельны или совпадают");
+else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+
+    Console.WriteLine($"Точка пересечения прямых имеет координаты: X {x} и Y {y}");
+}
